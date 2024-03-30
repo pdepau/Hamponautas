36e5f89922d976371fc49a73e3ade8351eab665b
pdepau <blanespau@gmail.com>
2024-03-30 22:33:42 +0100

    Generacion de terreno aleatoria

---
+++ hamponautas/Assets/Scripts/GeneradorT.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GeneradorT : MonoBehaviour
+{
+    public Pool ItemsPool;
+    // cola como el mercadona
+    Queue<Transform> elements;
+
+    public float speed;
+    public int Quantity = 30;
+    public float Displace = 15f;
+    public Vector3 direction;
+    public Vector3 offset;
+    Transform tr;
+
+    Vector3 originalPos;
+    float currentDisplace;
+    int moved = 0;
+    private void OnEnable()
+    {
+        ItemsPool.Initialize();
+        tr = transform;
+        elements = new Queue<Transform>();
+        for (int i = 0; i < Quantity; i++)
+        {
+           var elementTransform= ItemsPool.GetRandom();
+            elementTransform.position = offset - direction * Displace * i;
+            elementTransform.gameObject.SetActive(true);
+            elements.Enqueue(elementTransform);
+        }
+    }
+    private void Update()
+    {
+        tr.position += direction * speed * Time.deltaTime;
+
+        currentDisplace = Mathf.Abs(Vector3.Distance(tr.position, originalPos));
+        var timesToInfinite = currentDisplace / Displace;
+        if (timesToInfinite > moved +2) {
+            ToInfinite();
+        }
+    }
+
+    public void ToInfinite()
+    {
+        var last = elements.LastOrDefault();
+        var tel = elements.Dequeue();
+        tel.gameObject.SetActive(false);
+
+        var elementTransform = ItemsPool.GetRandom();
+        elementTransform.position = last.position - direction * Displace;
+        elementTransform.gameObject.SetActive(true);
+        elements.Enqueue(elementTransform);
+
+        moved++;
+    }
+}
---
+++ hamponautas/Assets/Scripts/Pool.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Pool : MonoBehaviour
+{
+    public List<PoolItem> Items;
+    public List<Transform> instanced = new List<Transform>();
+    public void Initialize()
+    {
+        Transform tr = transform;
+        foreach (PoolItem item in Items)
+        {
+            instanced.AddRange(item.Instantiate(tr));
+        }
+    }
+
+    public Transform GetRandom()
+    {
+        var actived = instanced.Where(t => !t.gameObject.activeSelf).ToArray();
+        return actived[Random.Range(0, actived.Length)];
+    }
+}
+[System.Serializable]
+public class PoolItem
+{
+    public Transform Prefab;
+    public int Quantity;
+    public Transform[] Instantiate(Transform parent)
+    {
+        Transform[] transforms = new Transform[Quantity];
+        for (int i=0; i< Quantity; i++)
+        {
+            transforms[i] = GameObject.Instantiate(Prefab, parent);
+            transforms[i].gameObject.SetActive(false);
+        }
+        return transforms;
+    }
+
+}
